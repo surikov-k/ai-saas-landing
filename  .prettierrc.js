626bd5ae08df33b4296a6e6356c705b4d574eb39
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-19 14:02:45 +0300

    Prettier

---
+++  .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  printWidth: 80,
+  tabWidth: 2,
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/sections/Header";
+
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
