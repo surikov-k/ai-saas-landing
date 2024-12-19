f5cea03d8c5aa0bb28c314091c7740f988b80fbf
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-19 15:23:12 +0300

    Hamburger Gradient Button

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -32,7 +32,7 @@ export const Header = () => {
   return (
     <header className="border-b border-gray-200/20">
       <div className="container">
-        <div className="flex h-18 items-center">
+        <div className="flex h-18 items-center justify-between">
           <div className="flex items-center gap-4">
             <div
               className={`
@@ -47,10 +47,27 @@ export const Header = () => {
             <div className="text-2xl font-extrabold">sphereal.ai</div>
           </div>
           <div></div>
-          <div>
-            <button>
-              <div></div>
-              <div></div>
+          <div className="flex items-center">
+            <button
+              className={`
+                relative size-10 rounded-lg border-2 border-transparent
+                [background:linear-gradient(var(--color-gray-950),var(--color-gray-950))_content-box,conic-gradient(var(--color-violet-400),var(--color-fuchsia-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400))_border-box]
+              `}
+            >
+              <div
+                className={`
+                  absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
+                `}
+              >
+                <div className="h-0.5 w-4 -translate-y-1 bg-gray-100"></div>
+              </div>
+              <div
+                className={`
+                  absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
+                `}
+              >
+                <div className="h-0.5 w-4 translate-y-1 bg-gray-100"></div>
+              </div>
             </button>
           </div>
         </div>
