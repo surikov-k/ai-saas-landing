9d00d12f2f18cba7a7157e9bcecaa85dff8e1811
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-19 14:50:49 +0300

    Gradient Logo

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,5 @@
+import logoImage from "@/assets/images/sphereal-logo.svg?url";
+
 export const navItems = [
   {
     name: "Features",
@@ -28,13 +30,31 @@ export const loginItems = [
 
 export const Header = () => {
   return (
-    <header
-      className={`
-        whitespace-nowrap px-4 py-4 text-zinc-400
-        hover:px-4
-      `}
-    >
-      header section
+    <header className="border-b border-gray-200/20">
+      <div className="container">
+        <div className="flex h-18 items-center">
+          <div className="flex items-center gap-4">
+            <div
+              className={`
+                size-10 bg-gray-200
+                bg-[conic-gradient(var(--color-violet-400),var(--color-fuchsia-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400))]
+              `}
+              style={{
+                maskImage: `url(${logoImage.src})`,
+                maskSize: "contain",
+              }}
+            ></div>
+            <div className="text-2xl font-extrabold">sphereal.ai</div>
+          </div>
+          <div></div>
+          <div>
+            <button>
+              <div></div>
+              <div></div>
+            </button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
