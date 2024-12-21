4bbcdc786dc50f567c95fff29b40d9d8c1a3a6fd
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-21 14:45:57 +0300

    Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Features from "@/sections/Features";
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
 import Pricing from "@/sections/Pricing";
+import Testimonials from "@/sections/Testimonials";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Companies />
       <Features />
       <Pricing />
+      <Testimonials />
     </>
   );
 }
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import SectionBorder from "@/components/SectionBorder";
+import SectionContent from "@/components/SectionContent";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 import React from "react";
 import AshwinSantiago from "@/assets/images/ashwin-santiago.jpg";
 import AlecWhitten from "@/assets/images/alec-whitten.jpg";
 import ReneWells from "@/assets/images/rene-wells.jpg";
 import MollieHall from "@/assets/images/mollie-hall.jpg";
+import { twMerge } from "tailwind-merge";
 
 export const testimonials = [
   {
@@ -35,8 +43,90 @@ export const testimonials = [
   },
 ];
 
+const SELECTED_TESTIMONIAL_INDEX = 0;
 export const Testimonials = () => {
-  return <section>testimonials section</section>;
+  // const [];
+  return (
+    <section>
+      <div className="container">
+        <SectionBorder borderTop>
+          <SectionContent>
+            <div
+              className={`
+                border-gradient relative flex flex-col gap-12 rounded-3xl px-6 py-16
+                lg:mx-20 lg:px-16 lg:py-24
+                md:mx-10 md:flex-row md:items-center md:px-10
+              `}
+            >
+              <FontAwesomeIcon
+                icon={faQuoteLeft}
+                className={`
+                  absolute left-6 top-0 size-20 -translate-y-1/2 text-violet-400
+                  lg:left-16
+                  md:left-10
+                `}
+              />
+              {testimonials.map(({ quote, name, title, image }, i) => (
+                <React.Fragment key={quote}>
+                  {i === SELECTED_TESTIMONIAL_INDEX && (
+                    <blockquote
+                      className={`
+                        flex flex-col gap-12
+                        lg:flex-row
+                      `}
+                    >
+                      <p
+                        className={`
+                          text-xl font-medium
+                          md:text-2xl
+                        `}
+                      >
+                        {quote}
+                      </p>
+                      <cite
+                        className={`
+                          not-italic
+                          lg:text-right
+                        `}
+                      >
+                        <Image
+                          src={image}
+                          alt={name}
+                          className="size-28 max-w-none rounded-xl"
+                        />
+                        <div className={`mt-4 font-bold`}>{name}</div>
+                        <div className={`mt-2 text-xs text-gray-400`}>
+                          {title}
+                        </div>
+                      </cite>
+                    </blockquote>
+                  )}
+                </React.Fragment>
+              ))}
+              <div
+                className={`
+                  flex justify-center gap-2
+                  md:flex-col
+                `}
+              >
+                {testimonials.map(({ quote }, index) => (
+                  <div
+                    key={quote}
+                    className={`relative isolate inline-flex size-6 items-center justify-center`}
+                  >
+                    {SELECTED_TESTIMONIAL_INDEX === index && (
+                      <div className="absolute border-gradient -z-10 size-full rounded-full"></div>
+                    )}
+                    <div className="size-1.5 rounded-full bg-gray-200"></div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </SectionContent>
+        </SectionBorder>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
