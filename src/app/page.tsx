3ca3fb8bcd571c89d503830a5b3ee67862abc66a
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-21 12:28:45 +0300

    Pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Companies from "@/sections/Companies";
 import Features from "@/sections/Features";
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
+import Pricing from "@/sections/Pricing";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Companies />
       <Features />
+      <Pricing />
     </>
   );
 }
--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,9 +1,17 @@
+import Button, { ButtonProps } from "@/components/Button";
+import SectionBorder from "@/components/SectionBorder";
+import SectionContent from "@/components/SectionContent";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { twMerge } from "tailwind-merge";
+
 export const pricingTiers = [
   {
     title: "Basic",
     description: "AI chatbot, personalized recommendations",
     price: "Free",
     buttonText: "Get Started",
+    buttonVariant: "secondary",
     features: [
       "Access to AI chatbot for natural language conversations",
       "Basic task automation for simple workflows",
@@ -17,6 +25,7 @@ export const pricingTiers = [
     description: "Advanced AI capabilities for enhanced productivity",
     price: 99,
     buttonText: "Upgrade to Premium",
+    buttonVariant: "secondary",
     features: [
       "All Basic features included",
       "Priority access to new AI features and updates",
@@ -42,10 +51,113 @@ export const pricingTiers = [
     color: "teal",
     className: "lg:py-12 lg:my-6",
   },
-];
+] satisfies {
+  title: string;
+  description: string;
+  price: number | string | null;
+  buttonText: string;
+  features: string[];
+  color: "violet" | "teal" | "amber";
+  className: string;
+  buttonVariant?: ButtonProps["variant"];
+}[];
 
 export const Pricing = () => {
-  return <section>Pricing Section</section>;
+  return (
+    <section>
+      <div className="container">
+        <SectionBorder borderTop>
+          <SectionContent>
+            <h2
+              className={`
+                text-center text-3xl font-semibold leading-tight text-gray-200
+                lg:text-5xl
+                md:text-4xl
+              `}
+            >
+              Flexible plans for every need
+            </h2>
+            <div
+              className={`
+                mt-12 flex flex-col gap-8
+                lg:flex-row lg:items-start
+              `}
+            >
+              {pricingTiers.map(
+                ({
+                  title,
+                  description,
+                  price,
+                  buttonText,
+                  buttonVariant,
+                  features,
+                  color,
+                  className,
+                }) => (
+                  <div
+                    key={title}
+                    className={twMerge(
+                      `
+                        mx-auto max-w-sm flex-1 rounded-3xl border border-[var(--color-border)] px-6
+                        py-12
+                      `,
+                      className,
+                    )}
+                  >
+                    <h3
+                      className={twMerge(
+                        "text-4xl font-semibold",
+
+                        color === "violet" && "text-violet-400",
+                        color === "amber" && "text-amber-300",
+                        color === "teal" && "text-teal-300",
+                      )}
+                    >
+                      {title}
+                    </h3>
+                    <p className="mt-4 text-gray-400">{description}</p>
+                    <div className="mt-8">
+                      {typeof price === "number" && (
+                        <span className="align-top text-2xl font-semibold text-gray-200">
+                          $
+                        </span>
+                      )}
+                      <span className="text-7xl font-semibold text-gray-200">
+                        {price ? price : <>&nbsp;</>}
+                      </span>
+                    </div>
+                    <Button
+                      variant={buttonVariant ? buttonVariant : "secondary"}
+                      className={`mt-8`}
+                      block
+                    >
+                      {buttonText}
+                    </Button>
+                    <ul className="mt-8 flex flex-col gap-4">
+                      {features.map((feature) => (
+                        <li
+                          key={feature}
+                          className={`
+                            flex items-start gap-4 border-t border-[var(--color-border)] pt-4
+                          `}
+                        >
+                          <FontAwesomeIcon
+                            icon={faCheckCircle}
+                            className="size-6 flex-shrink-0 text-violet-400"
+                          />
+                          <span className="font-medium">{feature}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ),
+              )}
+            </div>
+          </SectionContent>
+        </SectionBorder>
+      </div>
+    </section>
+  );
 };
 
 export default Pricing;
